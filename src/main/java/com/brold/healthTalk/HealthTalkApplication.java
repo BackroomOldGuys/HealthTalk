199695c831ee96d4e1a6cae4303f42119c6ded4e
kyusung Yoon <rbtjd0312@naver.com>
2025-06-27 19:46:49 +0900

    initial

---
+++ src/main/java/com/brold/healthTalk/HealthTalkApplication.java
@@ -0,0 +1,13 @@
+package com.brold.healthTalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthTalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brold/healthTalk/HealthTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brold.healthTalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthTalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
